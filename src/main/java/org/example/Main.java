8650025d55f146fcf8fcd2d3c2d9843252db6b26
Кристина <dragonfartune@gmail.com>
2025-08-23 18:49:37 +0300

    start

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import tasks.RomanToInteger;
+import tasks.ValidParentheses;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ValidParentheses.isValid("([])"));
+    }
+}
+
